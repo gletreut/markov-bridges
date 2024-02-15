b567f07c4f703e07e14f27cabee9609f2db723e1
Guillaume Le Treut <guillaume.letreut@czbiohub.org>
2024-02-15 11:40:10 -0800

    Changed the registration function to register one trajectory at a time. Also changed the returned output: now it simply gives the event index associated to each registered time.

--- mbridges/utils/processing.py
+++ mbridges/utils/processing.py
@@ -17,53 +17,45 @@ logging.basicConfig(format='%(asctime)s [%(filename)s:%(lineno)s] %(message)s', 
 #==============================================================================
 # functions
 #==============================================================================
-def register_trajectories(traj_list, tmin=0, tmax=None, dt=1.):
+def register_times(times, t0=0, tN=None, dt=1.):
   '''
   Register trajectories so that we obtain trajectories of coordinates at a regular time subdivision.
   INPUT:
-    * traj_list [N,d]: a list of N trajectories
+    * times [N,]: a list of N time series
     * tmin [float]: lower end of the time window
     * tmax [float]: upper end of the time window
     * dt [float]: time interval of the subdivision
   '''
+  Nt = len(times)
   # extract trajectories minimum and maximum times
-  Ts = []
-  for k,traj in enumerate(traj_list):
-    T = traj[[0,-1],0]
-    Ts.append(T)
-  Ts = np.concatenate(Ts).astype('float64')
-  Tsmin = np.min(Ts)
-  Tsmax = np.max(Ts)
+  tmin = np.min(times)
+  tmax = np.max(times)
 
   # define the registered times
   ## define bounds
-  if tmin is None:
-    tmin = Tsmin
-  if tmax is None:
-    tmax = Tsmax
-  if tmin > Tsmin:
-    logging.info("tmin = {:g} > min(T) = {:g}".format(tmin, Tsmin))
-  if tmax < Tsmax:
-    logging.info("tmax = {:g} < max(T) = {:g}".format(tmax, Tsmax))
+  if t0 is None:
+    t0 = tmin
+  if tN is None:
+    tN = np.ceil(tmax/dt)*dt # to ensure an integer number of bins
+  if t0 > tmin:
+    logging.info("t0 = {:g} > min(T) = {:g}".format(t0, tmin))
+  if tN < tmax:
+    logging.info("tN = {:g} < max(T) = {:g}".format(tN, tmax))
 
   ## define registered times
-  nbins = max(1, int((tmax-tmin) / dt))
-  dt = (tmax-tmin) / nbins # redefine dt to correspond to an integer number of bins
-  t_regis = tmin + np.arange(nbins+1, dtype='float64') * dt # regular time subdivision
-  logging.info("dt = {:g}  nbins = {:d}  tmin = {:g}  tmax = {:g}".format(dt, nbins, tmin, tmax))
+  nbins = max(1, int(np.ceil((tN-t0) / dt)))
+  tN_ = t0 + nbins*dt
+  if tN_ != tN:
+    logging.info("Adjusting tN from {:g} to {:g}".format(tN, tN_))
+    tN = tN_
+  # dt = (tmax-tmin) / nbins # redefine dt to correspond to an integer number of bins
+  t_regis = t0 + np.arange(nbins+1, dtype='float64') * dt # regular time subdivision
+  logging.info("dt = {:g}  nbins = {:d}  t0 = {:g}  tN = {:g}".format(dt, nbins, t_regis[0], t_regis[-1]))
 
   # create the registered trajectories
-  traj_regis_list = []
-  for traj in traj_list:
-    T = traj[:, 0]
-    states = traj[:, 1]
+  idx = np.digitize(t_regis, times)-1 # place registered times in bins such that t_{i-1} <= t_regis < t_{i} is associated with state i-1.
+  idx[t_regis < tmin] = 0 # registered times strictly smaller than smallest trajectory time are set in the initial state at i=0.
+  idx[~(t_regis < tmax)] = Nt-1 # registered times larger than the largest trajectory time are left in the final state at i = Nt - 1
 
-    idx = np.digitize(t_regis, T)-1 # place registered times in bins such that t_{i-1} <= t_regis < t_{i} is associated with state i-1.
-    idx[t_regis < T[0]] = 0 # registered times strictly smaller than smallest trajectory time are set in the initial state at i=0.
-    idx[~(t_regis < T[-1])] = len(T)-1 # registered times larger than the largest trajectory time are left in the final state at i = len(T) - 1
-
-    states_regis = states[idx] # get the values of the assigned states
-
-    traj_regis_list.append(states_regis)
-  return t_regis, np.array(traj_regis_list)
+  return t_regis, idx
 
